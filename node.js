b0cf282739a8cf1ef8b5945e445d293c633839aa
24LP3201 24LP3201 <24LP3201@G304-CC0219.local>
2024-12-13 12:17:23 +0800

    node.js

---
+++ node.js
@@ -0,0 +1,17 @@
+// const http = require("http");
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.write("Hello beauty!");
+//     res.end();
+//   })
+//   .listen(8080);
+
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Bottom to TOP!!");
+    res.end();
+  })
+  .listen(8080);
